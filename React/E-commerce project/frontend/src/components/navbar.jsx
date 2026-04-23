157116c9ab5b45aef0dd422d206954cbdb763ffd
Mohidul <islamohidul856mi647360@gmail.com>
2026-04-23 13:44:57 +0600

    updated navbar

--- React/E-commerce project/frontend/src/components/navbar.jsx
+++ React/E-commerce project/frontend/src/components/navbar.jsx
@@ -27,20 +27,19 @@ const Navbar= ()=>{
             </NavLink>
           </ul>
           <div className="flex items-center gap-6">
-             <img className="w-10 cursor-pointer" src={searchIcon} alt="search-icon" />
+             <img className="w-10 cursor-pointer" src={searchIcon} alt="search" />
              <div className="group relative">
               <img src={profileIcon} className="w-10 cursor-pointer" alt="profile" />
               <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-                <div className="flex flex-col gap-2 2-36 py-2 px-4 bg-slate-100 text-black">
-                  <p className="cursor-pointer hover:text-gray-500 font-semibold">Profile</p>
-                  <p className="cursor-pointer hover:text-gray-500 font-semibold">Orders</p>
-                  <p className="cursor-pointer hover:text-gray-500 font-semibold">Cart</p>
-                  <p className="cursor-pointer hover:text-gray-500 font-semibold">Logout</p>                  
+                <div className="flex flex-col gap-2 2-36 py-2 px-4 rounded-xl bg-slate-100 text-black">
+                  <NavLink to='/profile'><p className="cursor-pointer hover:text-gray-500 font-semibold">Profile</p></NavLink>               
+                  <NavLink to='/orders'><p className="cursor-pointer hover:text-gray-500 font-semibold">Orders</p></NavLink>               
+                  <NavLink to='/cart'><p className="cursor-pointer hover:text-gray-500 font-semibold">Cart</p></NavLink>               
+                  <NavLink to='/'><p className="cursor-pointer hover:text-gray-500 font-semibold">Logout</p></NavLink>               
                 </div>               
               </div>
              </div>
-              <img onClick={()=>setVisible(true)} src={menu} className="w-10 cursor-pointer sm:hidden" alt="" />
-              {/* sidebar menu for mobile/small screen */}
+              <img onClick={()=>setVisible(true)}  src={menu} className="w-10 cursor-pointer sm:hidden" alt="Menu" />
               {/* sidebar menu for mobile/small screen */}
               <div
                 className={`fixed top-0 right-0 bottom-0 w-64 bg-white shadow-lg transform transition-transform duration-300 ${
@@ -53,10 +52,10 @@ const Navbar= ()=>{
 
                 {/* Menu Links */}
                 <ul className="flex flex-col gap-6 px-6 text-black font-semibold">
-                  <NavLink to="/" onClick={() => setVisible(false)} className="hover:text-blue-500">HOME</NavLink>
-                  <NavLink to="/categories" onClick={() => setVisible(false)} className="hover:text-blue-500">CATEGORIES</NavLink>
-                  <NavLink to="/categories" onClick={() => setVisible(false)} className="hover:text-blue-500">ABOUT</NavLink>
-                  <NavLink to="/categories" onClick={() => setVisible(false)} className="hover:text-blue-500">CONTACT</NavLink>
+                  <NavLink to='/' onClick={() => setVisible(false)} className="hover:text-blue-500">HOME</NavLink>
+                  <NavLink to='/categories' onClick={() => setVisible(false)} className="hover:text-blue-500">CATEGORIES</NavLink>
+                  <NavLink to='/about' onClick={() => setVisible(false)} className="hover:text-blue-500">ABOUT</NavLink>
+                  <NavLink to='/contact' onClick={() => setVisible(false)} className="hover:text-blue-500">CONTACT</NavLink>
                 
                 </ul>
               </div>
